4b0314402e76bd62777a384e93967f3df33d203a
Kris Liang <1148995537@qq.com>
2024-08-15 15:51:30 +0800

    修复使用脚本工具请求时会出现连续两个 Domain 在 url 中

--- lib/network/components/script_manager.dart
+++ lib/network/components/script_manager.dart
@@ -316,7 +316,7 @@ async function onResponse(context, request, response) {
     });
     query = query.isEmpty ? query : '?${query.substring(0, query.length - 1)}';
 
-    request.uri = Uri.parse('${request.remoteDomain()}${map['path']}$query').toString();
+    request.uri = Uri.parse('${map['path']}?$query').toString();
 
     map['headers'].forEach((key, value) {
       if (value is List) {
